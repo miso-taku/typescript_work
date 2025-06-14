37022b4accfd9c52799f05c86b6ba4eb20311d0d
miso-taku <wk.ta93mis@gmail.com>
2025-06-14 16:11:55 +0900

    2ch commit

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-const message: string = "We are BABYMETAL!!!"
+let i : number = 1;
 
-console.log(message)
+while (i <= 100) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+    i++;
+}
