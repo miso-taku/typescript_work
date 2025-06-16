80dae16377acc8494b2acf19a9f17487f80a6a6d
miso-taku <wk.ta93mis@gmail.com>
2025-06-16 19:41:23 +0900

    3ch commit

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
-let i : number = 1;
+type User = {
+  name: string;
+  age: number;
+  premiumUser: boolean;
+};
 
-while (i <= 100) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      console.log("FizzBuzz");
-    } else if (i % 3 === 0) {
-      console.log("Fizz");
-    } else if (i % 5 === 0) {
-      console.log("Buzz");
-    } else {
-      console.log(i);
-    }
-    i++;
+const data: string = `
+Taro,26,1
+Jiro,18,0
+Saburo,32,0
+Ichiro,55,1
+`
+
+const users: User[] = data.split("\n").map((line) => {
+  const [name, ageString, premiumUserString] = line.split(",");
+  const age = Number(ageString);
+  const premiumUser = premiumUserString === "1";
+  return { name, age, premiumUser };
+});
+
+for (const user of users) {
+  if (user.premiumUser) {
+    console.log(`${user.name} (${user.age})はプレミアムユーザーです`);
+  } else {
+    console.log(`${user.name} (${user.age})はプレミアムユーザーではありません`);
+  }
 }
